5b4533ffa655becb4052a56bdef0dbdab612da53
benzwjian <zwjian@gmail.com>
2017-03-24 10:08:34 +0800

    add constant table

---
+++ src/const.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'SIGHASH': {
+    'ALL': 0x01,
+    'NONE': 0x02,
+    'SINGLE': 0x03,
+    'ALL|ANYONECANPAY': 0x81,
+    'NONE|ANYONECANPAY': 0x82,
+    'SINGLE|ANYONECANPAY': 0x83
+  }
+}
